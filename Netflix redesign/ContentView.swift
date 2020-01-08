7505b3993b8cb0a285b22dc90ba7b1814dfc3fb1
amtello <ytemilouwa@yahoo.com>
2020-01-08 23:06:34 +0100

    Initial Commit

---
+++ Netflix redesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netflix redesign
+//
+//  Created by Tello on 08/01/2020.
+//  Copyright © 2020 Tello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
